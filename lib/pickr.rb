f7e0d806390d52e3abdfd9d3215ee24e7773a0aa
Delon Newman <delon.newman@gmail.com>
2011-01-15 23:23:35 -1000

    doing a little refactoring

--- lib/pickr.rb
+++ lib/pickr.rb
@@ -1,11 +1,9 @@
+#
 # = Pickr - A Gallery tool for Photographers
 # These classes represent are an abstration away from the Flickr API.
 # They provide methods for creating a gallery of photos for selecting
 # and submitting to the photographer.
 #
-#	= Summary
-#
-#	Pickr::Gallery.get('26835295@N06').to_html
 
 require 'rubygems'
 require 'flickraw-cached'
@@ -28,36 +26,6 @@ module Pickr
 
 	FlickRaw.api_key    = API_KEY		
 
-	# TODO: move this stuff to lib/pickr/cache.rb
-	module Cache
-	
-		# TODO: add checking to opts
-		def read(type, *opts)
-			if type == :file
-				JSON.parse(IO.read(opts[:from]))
-			elsif type == :db
-				# return db cache
-				DB.read(opts[:from])
-			end
-		end
-
-		class DB
-			def self.read(table)
-
-			end
-		end
-
-	end
-	
-	# TODO: move this stuff to lib/pickr/db.rb 
-	module DB
-		# use DataMapper
-		# Models:
-		#  - PhotoSet
-		#  - Photo  
-	end
-
-	# A pickr photo set
 	class PhotoSet
 		attr_reader   :id, :description, :photos, :primary_photo_id	
 		attr_accessor :title
@@ -74,9 +42,7 @@ module Pickr
 		private
 		
 		def construct_photos(photos)
-			photos.map do |photo| 
-				Photo.new(photo.id, photo.title, photo.server, photo.secret)
-			end
+			photos.map {|p| Photo.new(p.id, p.title, p.server, p.secret) }
 		end
 
 		public
@@ -92,31 +58,10 @@ module Pickr
 			PhotoSet.new(info, set.photo)
 		end
 		
-		def photos_as_html(img_size)
-			photos.map { |photo| photo.to_html(img_size) }.join("")
+		def url
+			"#{FLICKR_PHOTO_URL}/#{USER_ID}/sets/#{@id}"
 		end
 	
-		def to_index_listing
-			return <<-HTML
-				<div id="set_#{@id}">
-					<a href="/#{@id}"><img src="#{primary_photo.url(:square)}" width="75" height="75" /></a>
-					<a href="/#{@id}">#{@title}</a>
-				</div>
-			HTML
-		end
-	
-		def to_html(img_size=SET_PHOTO_SIZE)
-			photo_list = photos_as_html(img_size)
-			return <<-HTML
-				<a href="/">&lt;&lt;Back to Index</a>
-				<h2>#{@title}</h2>
-				<p class="description">#{@description}</p>
-				<div class="photo-list">
-					#{photo_list}
-				</div>
-			HTML
-		end
-
 		def to_hash
 			{ id => primary_photo.to_square_url }
 		end
@@ -133,21 +78,20 @@ module Pickr
 			@secret = secret
 		end
 
-		def self.blank
-			Photo.new(nil, nil, nil, nil)
-		end
-	
 		def self.get(id)
 			photo = flickr.photos.getInfo :photo_id => id
 			Photo.new(id, photo.title, photo.server, photo.secret)
 		end
 	
-		# generates url for photo of type:
+		# 
+		# Generates url for photo of type:
 		# - square
 		# - thumbnail
 		# - original
 		# - medium
 		# - page
+		# - lightbox
+		# 
 		def url(type=SET_PHOTO_SIZE)
 			return @url unless @url.nil? # allows us to override url generation
 			case type
@@ -167,8 +111,9 @@ module Pickr
 	
 		private
 
+		# XXX: It seems there might be a more rubyish way of doing this
 		def to_base_url
-			@base_url ||= "#{FLICKR_STATIC_URL}/#{server}/#{id}_#{secret}"
+			@base_url ||= "#{FLICKR_STATIC_URL}/#{@server}/#{@id}_#{@secret}"
 		end
 
 		def to_square_url
@@ -188,25 +133,11 @@ module Pickr
 		end
 	
 		def to_page_url
-			@page_url ||= "#{FLICKR_PHOTO_URL}/#{USER_ID}/#{id}"
+			@page_url ||= "#{FLICKR_PHOTO_URL}/#{USER_ID}/#{@id}"
 		end
 
 		def to_lightbox_url
 			@lightbox_url ||= "#{to_page_url}/lightbox"
-		end
-
-		public
-	
-		def to_html(img_size=:thumb)
-			return <<-HTML
-				<div class="photo-region">
-					<a href="#{url(:page)}" target="__page-#{id}">
-						<img class="photo" id="photo_#{id}" src="#{url(img_size)}" title="#{title}" />
-					</a><br />
-					<input type="checkbox" name="photo" value="#{id}" />
-					<label for="photo_#{id}">Select</label>
-				</div>
-			HTML
 		end
 
 	end # Photo
@@ -219,32 +150,11 @@ module Pickr
 			@sets    = sets.map {|s| PhotoSet.new(s) }
 		end
 	
-		private 
-
-		def sets_as_html
-			@sets.map { |set| set.to_index_listing }.join("")
-		end
-
-		public
-	
 		def self.get(user_id)
 			sets = flickr.photosets.getList :user_id => user_id
 			self.new(user_id, sets)
 		end
 	
-		def to_html(title='Events')
-			return <<-HTML
-				<h1>#{title}</h1>
-				<div id="set-list">
-					#{sets_as_html}
-				</div>
-			HTML
-		end
-	
-		def self.show_set(set_id, img_size=:medium)
-			PhotoSet.get(set_id).to_html(img_size)
-		end
-
 		def to_hash(&block)
 			h = {}
 			sets.each do |s|
